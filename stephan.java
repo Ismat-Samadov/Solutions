07cfddf9ce10f943d3b24c137eb2b0d0f3dc4f5a
Ismat-Samadov <ismetsemedov@gmail.com>
2023-03-15 22:43:34 +0400

    stephan

---
+++ stephan.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class stephan {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int stones = scan.nextInt();
+        int friends = stones - 1;
+        System.out.println(friends);
+    }
+}
