86ca72fb239d003f7e905778fd7ea81906bcd783
Ismat-Samadov <ismetsemedov@gmail.com>
2023-03-19 18:05:26 +0400

    Biggest number without loop

--- buildings.java
+++ buildings.java
@@ -1,21 +1,15 @@
 import java.util.*;
 
 
+public class buildings {
 
-public class buildings
-
-{
-
-    public static void main(String []args)
-
-    {
+    public static void main(String[] args) {
 
         Scanner con = new Scanner(System.in);
 
         int n = con.nextInt();
 
         int m = con.nextInt();
-
 
 
         if ((n + m) % 2 == 0)
@@ -25,7 +19,6 @@ public class buildings
         else
 
             System.out.println("0");
-
 
 
         con.close();
