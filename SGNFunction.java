8f357f52b326e6298c4d8078b9061f33b2dd0b15
Ismat-Samadov <ismetsemedov@gmail.com>
2023-03-20 20:35:40 +0400

    WaterAndIce

--- SGNFunction.java
+++ SGNFunction.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ConditionalStatement_2 {
+public class SGNFunction {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int x = sc.nextInt();
