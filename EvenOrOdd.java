2f21413e6bf08f04f04e2b591969360b9cff3b03
Ismat-Samadov <ismetsemedov@gmail.com>
2023-03-21 20:47:47 +0400

    ExactlyOneConditionOutOfTwo

--- EvenOrOdd.java
+++ EvenOrOdd.java
@@ -4,7 +4,7 @@ public class EvenOrOdd {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int input = sc.nextInt();
-        if (input %2 ==0) {
+        if (input % 2 == 0) {
             System.out.println("EVEN");
         } else if (input != 0) {
             System.out.println("ODD");
