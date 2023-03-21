92c1d743044a9dd5ccb11a7dfae3dddc4cc602f1
Ismat-Samadov <ismetsemedov@gmail.com>
2023-03-21 15:01:27 +0400

    OneDigitNumber

--- NumbersOfTheSameSign.java
+++ NumbersOfTheSameSign.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ConditionalStatement_3 {
+public class NumbersOfTheSameSign {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         long n = sc.nextInt();
