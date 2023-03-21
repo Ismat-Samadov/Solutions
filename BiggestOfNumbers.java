2ba337b2da66290f74b5e93f6d4ed7d6cb6070eb
Ismat-Samadov <ismetsemedov@gmail.com>
2023-03-21 13:41:32 +0400

    Biggest number and divisivlity

--- BiggestOfNumbers.java
+++ BiggestOfNumbers.java
@@ -7,21 +7,8 @@ public class BiggestOfNumbers {
         int number2 = sc.nextInt();
         int number3 = sc.nextInt();
         int number4 = sc.nextInt();
-
-//        if (number1 < 1000 && number2 < 1000 && number3 < 1000 && number4 < 1000) {
-//            if (number1 > number2 && number1 > number3 && number1 > number4) {
-//                System.out.println(number1);
-//            } else if (number2 > number1 && number2 > number3 && number2 > number4) {
-//                System.out.println(number2);
-//            } else if (number3 > number1 && number3 > number2 && number3 > number4) {
-//                System.out.println(number3);
-//            } else {
-//                System.out.println(number4);
-//            }
-//        }
-//        else {System.out.println("out of range");}
-        
-        System.out.println(Math.max(Math.max(number1, number2), Math.max(number3, number4)));
+        System.out.println(Math.max(Math.max(number1, number2),
+                Math.max(number3, number4)));
         sc.close();
 
     }
