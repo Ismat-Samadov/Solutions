9535075780d8baa8290a14b7085453f98de9f5bc
Ismat-Samadov <ismetsemedov@gmail.com>
2023-03-22 18:02:33 +0400

    EachConditionOfOfTwo

--- EachConditionOfTwo.java
+++ EachConditionOfTwo.java
@@ -3,18 +3,13 @@ import java.util.Scanner;
 public class EachConditionOfOfTwo {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        int n = Math.abs(sc.nextInt());
 //        int length = (int) (Math.log10(n) + 1);
-        if (
-                n % 6 == 0 && length == 2 && (n > 9 && n < 100)
-        ) {
+        if (n % 6 == 0 && n > 9 && n < 100) {
             System.out.println("YES");
         } else {
             System.out.println("NO");
         }
-
-
         sc.close();
-
     }
 }
