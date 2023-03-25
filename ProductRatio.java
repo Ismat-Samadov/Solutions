02ce7b9fdb5e9cc700f127a2b1235ef659740c73
Ismat-Samadov <ismetsemedov@gmail.com>
2023-03-25 22:53:02 +0400

    ProductRatio

---
+++ ProductRatio.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ProductRatio {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int prod = 1;
+        double sum = 0;
+        while (n > 0) {
+            int temp = n % 10;
+            prod *= temp;
+            sum += temp;
+            n /= 10;
+        }
+        System.out.printf("%.3f", (prod / sum));
+    }
+}
