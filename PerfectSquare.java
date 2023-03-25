1decdc95fc7e95f767e83082e70a9c7aeaa7f6ed
Ismat-Samadov <ismetsemedov@gmail.com>
2023-03-25 23:34:42 +0400

    PerfectSquare

---
+++ PerfectSquare.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class PerfectSquare {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int sqrt = (int) Math.sqrt(n);
+        if (sqrt * sqrt == n) {
+            System.out.println(sqrt);
+        } else {
+            System.out.println("No");
+        }
+    }
+}
