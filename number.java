eab69ea0bb2001c813faee9880f7b12143dcfb89
Ismat-Samadov <ismetsemedov@gmail.com>
2023-03-26 16:49:30 +0400

    number

--- number.java
+++ number.java
@@ -3,19 +3,8 @@ import java.util.Scanner;
 public class number {
 
     public static void main(String[] args) {
-//        Scanner scan = new Scanner(System.in);
-//        int i = scan.nextInt();
-//
-//        int q = scan.nextInt();
-//        int s = scan.nextInt();
-//
-//
-//        // Write your code here.
-//
-//        System.out.println("String: " + i);
-//        System.out.println("Double: " + q);
-//        System.out.println("Int: " + s);
-        for (int i=1;i<6;i++){ System.out.println(i);
+        for (int i = 1; i < 6; i++) {
+            System.out.println(i);
         }
     }
 }
