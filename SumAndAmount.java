46a6fa9389d22b0fefa8424a8a8ce983947e41b0
Ismat-Samadov <ismetsemedov@gmail.com>
2023-03-26 19:45:17 +0400

    SumAndAmount

---
+++ SumAndAmount.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class SumAndAmount {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = 0;
+        int sum = 0;
+        while (scanner.hasNext()) {
+            int num = scanner.nextInt();
+            sum += num;
+            n++;
+        }
+        System.out.print(n + " " + sum);
+    }
+}
