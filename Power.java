b3745ea93f0976c79a70bf44d0ed1b7ce856addc
Ismat-Samadov <ismetsemedov@gmail.com>
2023-03-26 21:40:59 +0400

    Power

--- Power.java
+++ Power.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
-import java.lang.Math;
 
+import java.lang.Math;
 
 public class Power {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int n = sc.nextInt();
-        System.out.println(Math.pow(a, n));
-
+        int result = (int) Math.pow(a, n);
+        System.out.print(result);
         sc.close();
 
     }
