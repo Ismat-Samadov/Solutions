a2fe7601b7d3a4594b2ed87612f8023235f6c90c
Ismat-Samadov <ismetsemedov@gmail.com>
2023-03-26 18:58:12 +0400

    Sum of elements

---
+++ SumOfElements.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class SumOfElements {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        double sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += scanner.nextDouble();
+        }
+        System.out.printf("%.1f", sum);
+    }
+}
