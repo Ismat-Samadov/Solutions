3fc3eb8a29f3b1c70bcd90357f2c6fab5f1e9f54
Ismat-Samadov <ismetsemedov@gmail.com>
2023-06-09 17:43:18 +0400

    check

--- MatchModel.java
+++ MatchModel.java
@@ -4,6 +4,9 @@ public class MatchModel {
 
     Scanner scanner = new Scanner(System.in);
     int bricksCount = scanner.nextInt();
+    if( bricksCount==1 ){
+        System.out.println(12);
+    }
 }
 
 
